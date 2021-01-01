4597168df8f0dbfcb970054d26bcad2358b059d0
vicky7274 <vigneshwaran2747@gmail.com>
2021-01-01 15:27:10 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const app = express();
+
+const bodyParser = require('body-parser');
+const mongoose = require("mongoose");
+
+const productRoutes = require('./api/routes/products');
+
+//mongodb connection
+var mongoDB = 'mongodb://localhost/test';
+mongoose.connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true});
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+//handling cors
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    if (req.method === "OPTIONS") {
+        res.header("Access-Control-Allow-Methods", "PUT, POST, DELETE, GET");
+        return res.status(200).json({});
+    }
+    next();
+})
+
+app.use('/products', productRoutes);
+
+app.use((req, res, next) => {
+    console.log(next)
+    const error = new Error("requesting API not found");
+    error.status = 404;
+    next(error);
+});
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500)
+        .json({
+            error: {
+                message: error.message
+            }
+        })
+});
+
+module.exports = app;
